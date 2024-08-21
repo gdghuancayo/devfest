5f63e93d97bb05ab9ed8ab21372dd73b45ea0d2e
José David <70653346+josedrz@users.noreply.github.com>
2024-08-21 10:47:37 -0500

    Fix Import

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 import clsx from 'clsx'
 
-import backgroundImage from '@/images/background.jpg'
-
 export function BackgroundImage({ className, position = 'left' }) {
   return (
     <div
@@ -19,7 +17,7 @@ export function BackgroundImage({ className, position = 'left' }) {
           position === 'right' &&
             'left-full -translate-x-1/2 sm:left-1/2 sm:translate-x-[-20%] sm:translate-y-[-15%] md:translate-x-0 lg:translate-x-[5%] lg:translate-y-[4%] xl:translate-x-[27%] xl:translate-y-[-8%]',
         )}
-        src={backgroundImage}
+        src={""}
         alt=""
         width={918}
         height={1495}
